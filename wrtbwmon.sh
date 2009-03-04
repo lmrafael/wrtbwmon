0818e568092d7ec14ce2bbfaacb2d5e0da404c94
emmanuel.brucy <emmanuel.brucy@localhost>
2009-03-04 03:35:53 +0000

    Now entierely command-line driven, generate tables instead of graphs (rrdtool dumped), no awk dependency anymore.

--- wrtbwmon.sh
+++ wrtbwmon.sh
@@ -7,37 +7,33 @@
 # Based on work from Fredrik Erlandsson (erlis@linux.nu)
 # Based on traff_graph script by twist - http://wiki.openwrt.org/RrdTrafficWatch
 
-# TWEAKABLES
-
-#Should graphs be generated ? Needs rrdtool (installed through ipkg)
-USE_RRDTOOL="yes"
-
-#Day of the month the billing period resets
-BILLING_DAY=25
-
-#Peak & Offpeak times - don't forget the 0s
-BILLING_OFFPEAK_HOURS="04 05 06 07 08"
-
-#Where the main database will be stored - written to very often and must be persistant across
-#reboots in order for this tool to make sens. A cifs share is best suited.
-DBFILE="/tmp/rrdbw.db"
-
-#Where the RRD databases will be stored - written to very often and must be persistant across
-#reboots in order for this tool to make sens. A cifs share is best suited.
-RRD_DIR="/tmp/rrdbw"
-
-#A symlink to the graphs will be placed there - choose a path the internal web server has access to
-HTML_DIR="/tmp/rrdbw"
-
-#Some temporary files, does not matter if they are lost. Get written to pretty often.
-ARPFILE="/tmp/arpfile.log"
-
-#Interface names
-IF_LAN=`nvram get lan_ifname`
-
-
-updatefilters()
+formatnumber()
 {
+    kilo=$(echo $1 | sed 's/[^0-9]//g')
+    if [ -z "$kilo" ]; then
+		echo 0 >> $2
+	else
+	    mega=$(($kilo/1024))
+	    giga=$(($kilo/1048576))
+	    if [ $mega -lt 1 ] ; then
+		    echo "${kilo} k" >> $2
+	    elif [ $giga -lt 1 ] ; then
+		    echo "${mega} M" >> $2
+	    else
+		    megafrac=$(((($kilo-$(($giga\*1048576))))/1024))
+		    echo "${giga}.${megafrac} G" >> $2
+	    fi
+   fi
+}
+
+case $1 in
+
+"setup" )
+
+	[ -z "$2" ] && echo "Missing argument 2" && exit 1
+
+	touch /tmp/arpfile.log
+
 	#Create the RRDIPT CHAIN (it doesn't matter if it already exists).
 	iptables -N RRDIPT 2> /dev/null
 
@@ -49,7 +45,7 @@ updatefilters()
 	fi
 
 	#For each host in the ARP table
-	grep $IF_LAN /proc/net/arp | while read IP TYPE FLAGS MAC MASK IFACE
+	grep $2 /proc/net/arp | while read IP TYPE FLAGS MAC MASK IFACE
 	do
 		if [ ! $IP ]; then 
 			continue
@@ -57,13 +53,10 @@ updatefilters()
 
 		CURRHOST="$MAC $IP"
 
-		echo "Checking counters for $CURRHOST..."
-
 		#Is MAC is assigned to the same IP as last time ?
-		touch "$ARPFILE"
-		grep "$CURRHOST" "$ARPFILE" > /dev/null
+		grep "$CURRHOST" /tmp/arpfile.log > /dev/null
 		if [ $? -ne 0 ]; then
-			echo "New/modified entity : $MAC / $IP"
+			echo "DEBUG : New/modified entity : $MAC / $IP"
 			
 			#Add iptable rules (if non existing).
 			iptables -nL RRDIPT | grep $IP > /dev/null
@@ -73,187 +66,102 @@ updatefilters()
 			fi
 
 			#Update the ARP file
-			grep -v "$MAC" "$ARPFILE" | grep -v $IP > "$ARPFILE.new"
-			mv "$ARPFILE.new" "$ARPFILE"
-			echo ${CURRHOST} >> "$ARPFILE"
+			grep -v "$MAC" /tmp/arpfile.log | grep -v $IP > /tmp/arpfile.new
+			mv /tmp/arpfile.new /tmp/arpfile.log
+			echo ${CURRHOST} >> /tmp/arpfile.log
 		fi
 	done	
-}
-
-
-createDb()
-{
-	mkdir -p $RRD_DIR
-	rrdtool create "$RRD_DIR/$1" \
-		--start `date +%s` \
-		--step 300 \
-		DS:in_peak:ABSOLUTE:600:0:U \
-		DS:out_peak:ABSOLUTE:600:0:U \
-		DS:in_offpeak:ABSOLUTE:600:0:U \
-		DS:out_offpeak:ABSOLUTE:600:0:U \
-		RRA:AVERAGE:0.5:1:288 \
-		RRA:AVERAGE:0.5:288:31
-}
-
-updategraphs()
-{
-	echo "Updating graphs..."
-	touch $DBFILE
+	;;
+	
+"update" )
+	[ -z "$2" ] && echo "Missing argument 2" && exit 1
+	touch $2
 
 	#Read and reset counters
 	iptables -L RRDIPT -vnxZ -t filter > /tmp/traffic.tmp
 
-	cat "$ARPFILE"| while read MAC IP
+	cat /tmp/arpfile.log| while read MAC IP
 	do
-		#Add new data to the graph.
-		IN=$(cat /tmp/traffic.tmp|awk "{if (\$8 == \"$IP\") print \$2}" | tr -d '\n')
-		OUT=$(cat  /tmp/traffic.tmp|awk "{if (\$9 == \"$IP\") print \$2}" | tr -d '\n')
-		echo "New traffic for $MAC since last update : $IN:$OUT"
+		#Add new data to the graph. Count in Kbs to deal with 16 bits signed values (up to 2G only)
+		#Have to use temporary files because of crappy busybox shell
+		grep $IP /tmp/traffic.tmp | while read PKTS BYTES TARGET PROT OPT IFIN IFOUT SRC DST
+		do
+			[ "$SRC" = "$IP" ] && echo $(($BYTES/1024)) > /tmp/in.tmp
+			[ "$DST" = "$IP" ] && echo $(($BYTES/1024)) > /tmp/out.tmp
+		done
 		
-		PEAKUSAGE_IN=$(grep $MAC $DBFILE | awk '{print $2}')
-		PEAKUSAGE_OUT=$(grep $MAC $DBFILE | awk '{print $3}')
-		OFFPEAKUSAGE_IN=$(grep $MAC $DBFILE | awk '{print $4}')
-		OFFPEAKUSAGE_OUT=$(grep $MAC $DBFILE | awk '{print $5}')		
+		IN=$(cat /tmp/in.tmp)
+		OUT=$(cat /tmp/out.tmp)
 		
-		if [ "$USE_RRDTOOL" = "yes" ]; then
-			GRAPH="$RRD_DIR/$MAC.rrd"
-			if [ ! -f "$GRAPH" ]; then
-				echo "$GRAPH is missing, recreating it"
-				createDb "$MAC.rrd"
+		if [ $IN -gt 0 -o $OUT -gt 0 ];  then
+			echo "New traffic for $MAC since last update : $IN:$OUT"
+		
+			PEAKUSAGE_IN=$(grep $MAC $2 | cut -f2 -s -d, )
+			PEAKUSAGE_OUT=$(grep $MAC $2 | cut -f3 -s -d, )
+			OFFPEAKUSAGE_IN=$(grep $MAC $2 | cut -f4 -s -d, )
+			OFFPEAKUSAGE_OUT=$(grep $MAC $2 | cut -f5 -s -d, )		
+
+			if [ "$3" = "offpeak" ]; then
+				OFFPEAKUSAGE_IN=$(($OFFPEAKUSAGE_IN+$IN))
+				OFFPEAKUSAGE_OUT=$(($OFFPEAKUSAGE_OUT+$OUT))
+			else
+				PEAKUSAGE_IN=$(($PEAKUSAGE_IN+$IN))
+				PEAKUSAGE_OUT=$(($PEAKUSAGE_OUT+$OUT))
 			fi
+
+			grep -v "$MAC" $2 > /tmp/db.new
+			mv /tmp/db.new $2
+			echo $MAC,$PEAKUSAGE_IN,$PEAKUSAGE_OUT,$OFFPEAKUSAGE_IN,$OFFPEAKUSAGE_OUT,$(date "+%d-%m-%Y %H:%M") >> $2
 		fi
-		
-		echo "$BILLING_OFFPEAK_HOURS" | grep `date +%H`  > /dev/null
-		if [ $? -ne 0 ]; then
-			#Peak hour
-			PEAKUSAGE_IN=$(($PEAKUSAGE_IN+$IN))
-			PEAKUSAGE_OUT=$(($PEAKUSAGE_OUT+$OUT))
-
-			[ "$USE_RRDTOOL" = "yes" ] && rrdtool update "$GRAPH" N:$IN:$OUT:0:0
-		else
-			#Offpeak hour
-			OFFPEAKUSAGE_IN=$(($OFFPEAKUSAGE_IN+$IN))
-			OFFPEAKUSAGE_OUT=$(($OFFPEAKUSAGE_OUT+$OUT))
-
-			[ "$USE_RRDTOOL" = "yes" ] && rrdtool update "$GRAPH" N:0:0:$IN:$OUT
-		fi
-
-		grep -v "$MAC" "$DBFILE" > /tmp/db.new
-		mv /tmp/db.new "$DBFILE"
-		echo $MAC $PEAKUSAGE_IN $PEAKUSAGE_OUT $OFFPEAKUSAGE_IN $OFFPEAKUSAGE_OUT >> "$DBFILE"
-
 	done
 	
 	#Free some memory
 	rm /tmp/traffic.tmp
-}
-
-# $1 = ImageFile, $2 = Start Time, $3 = End Time, $4 = RRDfile, $5 = GraphText
-CreateGraph ()
-{
-	#Escape the : in the file name
-	F=$(echo $4| sed 's/:/\\:/g')
-
-        rrdtool graph "$1" -a PNG -s $2 -e $3 -w 550 -h 240 -v "bytes/s" -t "${5}" --lazy \
-                DEF:in_peak="$F":in_peak:AVERAGE AREA:in_peak#FF0000:"Download (peak)" LINE1:in_peak#000000 \
-                DEF:out_peak="$F":out_peak:AVERAGE AREA:out_peak#FFFF00:"Upload (peak)" LINE1:out_peak#000000 \
-                DEF:in_offpeak="$F":in_offpeak:AVERAGE AREA:in_offpeak#00FF00:"Download (offpeak)" LINE1:in_offpeak#000000 \
-                DEF:out_offpeak="$F":out_offpeak:AVERAGE AREA:out_offpeak#0000FF:"Upload (offpeak)" LINE1:out_offpeak#000000
-}
-
-draw_graphic()
-{
-	F=$(echo $6| sed 's/:/\\:/g')
+	rm /tmp/in.tmp
+	rm /tmp/out.tmp
+	;;
 	
-	echo "Generating: $1"
-	
-	rrdtool graph "$1" -s $2 -e $3 -a PNG\
-	-t "Net usage for $4" \
-	-h 140 -w 600 \
-	-l 1 \
-	--logarithmic \
-	-r \
-	-v bits/sec \
-	--lazy \
-	DEF:in_="$RRD_DIR/$F.rrd":in:AVERAGE \
-	DEF:out_="$RRD_DIR/$F.rrd":out:AVERAGE \
-	CDEF:in=in_,8,* \
-	CDEF:out=out_,8,* \
-	CDEF:eth0_bytes_in=in_,$2,-1,*,* \
-	CDEF:eth0_bytes_out=out_,$2,-1,*,* \
-	CDEF:eth0_bytes=eth0_bytes_in,eth0_bytes_out,+ \
-	AREA:in#32CD32:Incoming \
-	LINE1:in#336600 \
-	GPRINT:in:MAX:'Max\: %5.1lf %s' \
-	GPRINT:in:AVERAGE:'Avg\: %5.1lf %S' \
-	GPRINT:in:LAST:'Current\: %5.1lf %Sbits/sec' \
-	GPRINT:eth0_bytes_in:AVERAGE:'Total\: %7.2lf %sB\\n' \
-	LINE1:out#0033CC:Outgoing \
-	GPRINT:out:MAX:'Max\: %5.1lf %s' \
-	GPRINT:out:AVERAGE:'Avg\: %5.1lf %S' \
-	GPRINT:out:LAST:'Current\: %5.1lf %Sbits/sec' \
-	GPRINT:eth0_bytes_out:AVERAGE:'Total\: %7.2lf %sB\\n' \
-	COMMENT:" \n" \
-	GPRINT:eth0_bytes:AVERAGE:'Input + Output\: %7.2lf %sB' \
-	COMMENT:" \n" \
-	COMMENT:"$5\n"
-	#CDEF:eth0_bytes_out=out_,0,12500000,LIMIT,UN,1,out_,IF,86400,* \
-}
+"publish" )
 
-publishgraphs()
-{
-	#Values for the daily graph
-	NOW=`date +%s`
-	ONE_DAY_AGO=$(($NOW-86400))
+	[ -z "$2" ] && echo "Missing argument 2" && exit 1
+	[ -z "$3" ] && echo "Missing argument 3" && exit 1
+	[ -z "$4" ] && USERSFILE="/dev/null" || USERSFILE=$4
 
-	#Values for the billing graph
-	MONTH=`date +%m`
-	YEAR=`date +%Y`
-	CLOSEST=`date -d ${MONTH}250000${YEAR} +%s`
-	if [ $NOW -ge $CLOSEST ]; then
-		#Second half
-		MONTH=`awk 'BEGIN {printf("%0.2d\n", '$(expr $MONTH + 1)')}'` # Good thing date accepts 13th month !
-		CURRENT_BILLING_START=$CLOSEST
-		CURRENT_BILLING_STOP=`date -d ${MONTH}250000${YEAR} +%s`
-	else
-		#First half
-		MONTH=`awk 'BEGIN {printf("%0.2d\n", '$(expr $MONTH - 1)')}'`  # Hopefully date accepts if month = 0 too !
-		CURRENT_BILLING_START=`date -d ${MONTH}250000${YEAR} +%s`
-		CURRENT_BILLING_STOP=$CLOSEST
-	fi
-	
 	# create HTML page
-	echo "<html><head><title>Traffic</title></head><body>" > $HTML_DIR/index.htm
-	echo "<h1>Traffic</h1>" >> $HTML_DIR/index.htm
-	echo "This page was generated on `date`" >> $HTML_DIR/index.htm
-
-	if [ "$USE_RRDTOOL" = "yes" ]; then
-		for FILE in $RRD_DIR/*.rrd
+	echo "<html><head><title>Traffic</title></head><body>" > $3
+	echo "<h1>Total Usage :</h1>" >> $3
+	echo "<table border="1"><tr bgcolor=silver><td>User</td><td>Peak download</td><td>Peak upload</td><td>Offpeak download</td><td>Offpeak upload</td><td>Last seen</td></tr>" >> $3
+	cat $2 | while IFS=, read MAC PEAKUSAGE_IN PEAKUSAGE_OUT OFFPEAKUSAGE_IN OFFPEAKUSAGE_OUT LASTSEEN
 		do
-			PREFIX=`basename $FILE .rrd`
-	#		draw_graphic "${PREFIX}_last_day.png" $ONE_DAY_AGO $NOW "$PREFIX (last 24 hours)" "$TIMESTAMP" $FILE
-			CreateGraph "$HTML_DIR/${PREFIX}_last_day.png" $ONE_DAY_AGO $NOW $FILE "Usage for the last 24 hours"
-			CreateGraph "$HTML_DIR/${PREFIX}_billing.png" $CURRENT_BILLING_START $CURRENT_BILLING_STOP $FILE "Usage for the current billing period"
-			echo "<br><h2>Usage for ${PREFIX} :</h2><br>" >> $HTML_DIR/index.htm
-			echo "<img src='${PREFIX}_last_day.png' />" >> $HTML_DIR/index.htm
-			echo "<img src='${PREFIX}_billing.png' />" >> $HTML_DIR/index.htm
+			USER=$(grep $MAC $USERSFILE | cut -f2 -s -d= )
+			[ -z "$USER" ] && USER=$MAC
+			echo "<tr><td>$USER</td><td>" >> $3
+			formatnumber "$PEAKUSAGE_IN" $3
+			echo "</td><td>" >> $3
+			formatnumber "$PEAKUSAGE_OUT" $3
+			echo "</td><td>" >> $3
+			formatnumber "$OFFPEAKUSAGE_IN" $3
+			echo "</td><td>" >> $3
+			formatnumber "$OFFPEAKUSAGE_OUT" $3
+			echo "</td><td>" >> $3
+			echo "$LASTSEEN" >> $3
+			echo "</td></tr>" >> $3
 		done
-	fi
-	echo "</body></html>" >> $HTML_DIR/index.htm
-}
+	echo "</table>" >> $3
+	echo "<br /><small>This page was generated on `date`</small>" >> $3
+	echo "</body></html>" >> $3
+	;;
 
-case $1 in
-"updatefilters" )
-	updatefilters
-	;;
-"updategraphs" )
-	updategraphs
-	;;
-"publishgraphs" )
-	publishgraphs
-	;;
 *)
-	echo "TODO : help file"
+	echo "Usage : $0 {setup|update|publish} [options...]"
+	echo "Options : "
+	echo "   $0 setup lan_interface"
+	echo "   $0 update database_file [offpeak]"
+	echo "   $0 publish database_file path_of_html_report [user_file]"
+	echo "Examples : "
+	echo "   $0 setup br0"
+	echo "   $0 update /jffs/wrtbwmon.db offpeak"
+	echo "   $0 publish /jffs/wrtbwmon.db /www/user/usage.htm /jffs/wrtdwmonusers.txt"
 	exit
 	;;
 esac
